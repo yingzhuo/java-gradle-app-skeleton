ba72d24d90a1c74853129390c884eae30d043ad1
应卓 <yingzhor@gmail.com>
2025-07-31 07:56:19 +0800

    2025-07-31 07:56:19

--- projects-app/core/src/main/java/io/github/yingzhuo/showcase/core/ApplicationBoot.java
+++ projects-app/core/src/main/java/io/github/yingzhuo/showcase/core/ApplicationBoot.java
@@ -4,16 +4,19 @@
  */
 package io.github.yingzhuo.showcase.core;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ApplicationBoot {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		init();
+		SpringApplication.run(ApplicationBoot.class, args);
+	}
+
+	private static void init() throws Exception {
 		System.setProperty("spring.devtools.restart.enabled", "true");
-		new SpringApplicationBuilder(ApplicationBoot.class)
-			.run(args);
 	}
 
 }

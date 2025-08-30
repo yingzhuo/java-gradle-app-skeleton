3b5dc172abd4b36a7b872a8cdc24c1fca1ab8cb2
应卓 <yingzhor@gmail.com>
2025-08-30 13:57:43 +0800

    2025-08-30 13:57:43

--- projects-lib/addon-web-security/src/main/java/io/github/yingzhuo/showcase/websecurity/filter/LoggingFilter.java
+++ projects-lib/addon-web-security/src/main/java/io/github/yingzhuo/showcase/websecurity/filter/LoggingFilter.java
@@ -4,25 +4,29 @@
  */
 package io.github.yingzhuo.showcase.websecurity.filter;
 
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
-public class LoggingFilter extends OncePerRequestFilter {
-
-	private static final String BAR = "-".repeat(80);
+@RequiredArgsConstructor
+public class LoggingFilter extends OncePerRequestFilter implements Filter {
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+	@SneakyThrows
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
 		log.debug("-".repeat(80));
+		log.debug("[request timestamp]: '{}']", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
 		log.debug("[request path]: '{}'", request.getRequestURI());
-		log.debug("[request method]: {}]", request.getMethod());
+		log.debug("[request method]: '{}']", request.getMethod());
 		log.debug("-".repeat(80));
 		filterChain.doFilter(request, response);
 	}
